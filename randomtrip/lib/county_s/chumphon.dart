e8ed2b55b032c4de160d4d6c33723d37b4b03277
tawatchai03 <m.tawatchai03@gmail.com>
2023-09-15 10:45:52 +0700

    v
    v

--- randomtrip/lib/county_s/chumphon.dart
+++ randomtrip/lib/county_s/chumphon.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
 
-class AmnatcharoenPage extends StatelessWidget {
+class chumphonPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return _AmnatcharoenPageState();
+    return _chumphonPageState();
   }
 }
 
-class _AmnatcharoenPageState extends StatelessWidget {
+class _chumphonPageState extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('จังหวัดอำนาจเจริญ'),
+        title: Text('จังหวัดชุมพร'),
       ),
       body: Center(
-        child: Text('หน้านี้เป็นหน้าของจังหวัดอำนาจเจริญ'),
+        child: Text('หน้านี้เป็นหน้าของจังหวัดชุมพร'),
       ),
     );
   }
--- randomtrip/lib/county_s/krabi.dart
+++ randomtrip/lib/county_s/krabi.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
 
-class AmnatcharoenPage extends StatelessWidget {
+class krabiPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return _AmnatcharoenPageState();
+    return _krabiPageState();
   }
 }
 
-class _AmnatcharoenPageState extends StatelessWidget {
+class _krabiPageState extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('จังหวัดอำนาจเจริญ'),
+        title: Text('จังหวัดกระบี่'),
       ),
       body: Center(
-        child: Text('หน้านี้เป็นหน้าของจังหวัดอำนาจเจริญ'),
+        child: Text('หน้านี้เป็นหน้าของจังหวัดกระบี่'),
       ),
     );
   }
